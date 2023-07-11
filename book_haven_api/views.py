1a714253fb792de1c11265fcfe6ef883514b0d59
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-07-10 22:05:32 -0600

    feat: Add destroy_book function.

--- book_haven_api/views.py
+++ book_haven_api/views.py
@@ -1,4 +1,4 @@
-from .models import Library
+from .models import Library, Book
 from .serializers import LibrarySerializer, BookSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -30,3 +30,16 @@ def get_library_books(request, library_id):
     books = library.book_set
     serializer = BookSerializer(books, many=True)
     return Response({ "data": serializer.data }, status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+def destroy_book(request, library_id, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Book.DoesNotExist:
+        return Response({"errors": [{
+            "status": "404",
+            "title": "Book not found",
+            "detail": "The book with id " + str(book_id) + " does not exist."
+        }]},status=status.HTTP_404_NOT_FOUND)
