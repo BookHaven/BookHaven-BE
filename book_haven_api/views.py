136dbd597371f1c0dcb4af7e3a941f20540cc8da
Grace Joh <graceehjoh@gmail.com>
2023-07-10 19:41:04 -0500

    feat: create get_all_libraries request in views
    Co-authored by: Andrew Bingham <119075417+andrew-bingham1@users.noreply.github.com>
    Co-authored by: Alec Kapicak <120225785+AlecKap@users.noreply.github.com>
    Co-authored by: Thomas Hawley <120520954+thawley2@users.noreply.github.com>

--- book_haven_api/views.py
+++ book_haven_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Library
+from .serializers import LibrarySerializer
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def get_all_libraries(request):
+    libraries = Library.objects.all()
+    serializer = LibrarySerializer(libraries, many=True)
+    return Response({ "data": serializer.data }, status=status.HTTP_200_OK)
