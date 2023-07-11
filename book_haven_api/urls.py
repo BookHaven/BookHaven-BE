ba0c97828263d3577628ca1770271e6ac8b0e93e
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-07-10 22:04:52 -0600

    path: Add destroy_book path.

--- book_haven_api/urls.py
+++ book_haven_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('api/v0/libraries/', views.get_all_libraries, name='get_all_libraries'),
     path('api/v0/libraries/<int:library_id>/', views.get_a_library, name='get_a_library'),  # noqa: E501
     path('api/v0/libraries/<int:library_id>/books/', views.get_library_books, name='get_library_books'),  # noqa: E501
+    path('api/v0/libraries/<int:library_id>/books/<int:book_id>', views.destroy_book, name='destroy_book'),  # noqa: E501
 ]
