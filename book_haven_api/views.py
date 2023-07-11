1655b165d9ff56db64b466ca29f2a88f39d1e633
Thomas Hawley <thawley22@turing.edu>
2023-07-10 21:16:35 -0600

    feat: Add view for library books

--- book_haven_api/views.py
+++ book_haven_api/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from .models import Library
-from .serializers import LibrarySerializer
-from django.http import JsonResponse
+from .serializers import LibrarySerializer, BookSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
@@ -26,3 +24,9 @@ def get_a_library(request, library_id):
             "detail": "The library with id " + str(library_id) + " does not exist."
         }]},status=status.HTTP_404_NOT_FOUND)
     
+@api_view(['GET'])
+def get_library_books(request, library_id):
+    library = Library.objects.get(id=library_id)
+    books = library.book_set
+    serializer = BookSerializer(books, many=True)
+    return Response({ "data": serializer.data }, status=status.HTTP_200_OK)
