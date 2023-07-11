3f5aabaf3e169e125e88c77dd6d61d4784a088b0
Andrew Bingham <andrewgbingham@gmail.com>
2023-07-10 20:14:15 -0600

    feat: create get_a_library path

--- book_haven_api/urls.py
+++ book_haven_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('libraries/', views.get_all_libraries, name='get_all_libraries'),
+    path('api/v0/libraries/', views.get_all_libraries, name='get_all_libraries'),
+    path('api/v0/libraries/<int:library_id>/', views.get_a_library, name='get_a_library'),
 ]
